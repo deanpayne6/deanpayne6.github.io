15b29f04c237a0c207fee14efb0df17dfc7af7ab
dean <deandeanpayne@gmail.com>
2023-09-29 13:31:56 -0700

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app-routing.module.ts 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	new file:   src/app/portfolio-page/portfolio-page.component.css 	new file:   src/app/portfolio-page/portfolio-page.component.html 	new file:   src/app/portfolio-page/portfolio-page.component.spec.ts 	new file:   src/app/portfolio-page/portfolio-page.component.ts 	modified:   src/index.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:PortfolioPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
